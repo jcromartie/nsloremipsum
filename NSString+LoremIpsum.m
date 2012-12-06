3ea86f5ea80b67f965d95d6ce2f7fa4606654ded
John Cromartie <jcromartie@gmail.com>
2012-12-06 17:44:05 -0500

    Update NSString+LoremIpsum.m
    Fix import statement (previously refuctored)

--- NSString+LoremIpsum.m
+++ NSString+LoremIpsum.m
@@ -3,7 +3,7 @@
 //  Created by John Cromartie on 12/6/12.
 //
 
-#import "NSLoremIpsum.h"
+#import "NSString+LoremIpsum.h"
 
 static NSString *ipsum = @"lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum";
 
